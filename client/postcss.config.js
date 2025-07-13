086e443b4577cac154ead660d8bee36b11c07297
Tyson Lerfald <tyson@yobot.bot>
2025-07-13 02:34:09 -0700

    Fix client build

--- client/postcss.config.js
+++ client/postcss.config.js
@@ -1,6 +1,6 @@
+import tailwindcss from "tailwindcss";
+import autoprefixer from "autoprefixer";
+
 export default {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-}
+  plugins: [tailwindcss({ config: './tailwind.config.ts' }), autoprefixer()],
+};
---
+++ client/src/App.tsx
@@ -0,0 +1,5 @@
+import CommandCenter from "./pages/Command-Center/command-center";
+export default function App() {
+  return <CommandCenter />;
+}
+
