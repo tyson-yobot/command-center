662f438d58e3da79e4c70eb3b59cf8b8fdccd4c1
Daniel <daniel@yobot.bot>
2025-07-20 17:55:01 -0700

    Error fixing routing in main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,26 +1,22 @@
-
- // client/src/main.tsx
+// client/src/main.tsx
 import ReactDOM from "react-dom/client";
-import { StrictMode } from "react";import { Router, Route, Switch } from "wouter";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { StrictMode } from "react";
+import { useRoute } from "wouter";
 
 import CommandCenter from "@/pages/Command-Center/command-center";
-import "./index.css"; // or "./global.css" if you're using that instead
+import "./index.css";
 
-const queryClient = new QueryClient();const root = document.getElementById("root") as HTMLElement;
+const root = document.getElementById("root") as HTMLElement;
+
+function App() {
+  const [isRoot] = useRoute("/");
+  const [isCommandCenter] = useRoute("/command-center");
+  
+  return <CommandCenter />;
+}
 
 ReactDOM.createRoot(root).render(
   <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <Router>
-        <Switch>
-          <Route path="/" component={CommandCenter} />
-          <Route path="/command-center" component={CommandCenter} />
-          {/* <Route path="/lead-scraper" component={LeadScraper} /> */}
-          <Route>
-            <CommandCenter />
-          </Route>
-        </Switch>
-      </Router>    </QueryClientProvider>
+    <App />
   </StrictMode>
 );
